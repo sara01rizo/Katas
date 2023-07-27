522c444fc39bc57a083a8c96c4a65c76d477700f
sara pulido <sara01rizo@gmail.com>
2023-07-27 10:52:39 +0200

    Add: derivative js file

---
+++ derivative.js
@@ -0,0 +1,16 @@
+// Solution V1
+function derivative(b, m) {
+  return b * m ** (b - 1);
+}
+
+console.log(derivative(1, 4));
+console.log(derivative(3, -2));
+console.log(derivative(4, -3));
+console.log(derivative(9, -5));
+console.log(derivative(1254, 0));
+console.log(derivative(-2, 10));
+
+// Solution V2
+// const derivative = (b, m) => {
+//   return b * Math.pow(m, b - 1);
+// };
