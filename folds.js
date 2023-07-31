1aa0b97777ef0a81d83ee0988caa6cc06a7e2765
sara pulido <sara01rizo@gmail.com>
2023-07-31 16:15:42 +0200

    Add: folds js file

---
+++ folds.js
@@ -0,0 +1,22 @@
+// Solution V1
+const folds = (n) => `${2 ** (n - 1) / 1000}m`;
+
+console.log(folds(0));
+console.log(folds(1));
+console.log(folds(2));
+console.log(folds(3));
+console.log(folds(4));
+console.log(folds(5));
+console.log(folds(12));
+console.log(folds(13));
+console.log(folds(14));
+console.log(folds(15));
+console.log(folds(16));
+console.log(folds(28));
+console.log(folds(46));
+console.log(folds(49));
+console.log(folds(50));
+
+// function folds(n) {
+//   return `${(0.5 * Math.pow(2, n)) / 1000}m`;
+// }
