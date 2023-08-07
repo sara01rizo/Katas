6cc39d6e89cef04d2d57ddf6dc1a5c10f05cee58
sara pulido <sara01rizo@gmail.com>
2023-08-07 21:08:20 +0200

    Add: legendere push js file

--- legendere.js
+++ legendere.js
@@ -1,40 +1,39 @@
 // Solution V1
-let lcm = (n1, n2) => {
-  // Find the smallest and biggest number from both the numbers
-  let lar = Math.max(n1, n2);
-  let small = Math.min(n1, n2);
-
-  // Loop till you find a number by adding the largest number
-  // which is divisible by the smallest number
-  let i = lar;
-  while (i % small !== 0) {
-    i += lar;
+const legendre = (p, n) => {
+  let arr = [];
+  for (i = 1; p ** i <= n; i++) {
+    arr.push(~~(n / p ** i));
   }
-
-  return i;
+  return arr.reduce((acc, cur) => acc + cur, 0);
 };
 
-console.log(lcm(9, 18));
-console.log(lcm(8, 5));
-console.log(lcm(17, 11));
-console.log(lcm(17, 5));
-console.log(lcm(3, 12));
-console.log(lcm(9, 9));
+console.log(legendre(5, 100), 24);
 
-// const lcm = (n1, n2) => {
-//   let max = Math.max(n1, n2);
-//   let min = Math.min(n1, n2);
+console.log(legendre(2, 128), 127);
 
-//   let minArr = [];
-//   let maxArr = [];
+console.log(legendre(3, 50), 22);
 
-//   let count = 1;
-//   while (minArr.some((item) => maxArr.includes(item)) === false) {
-//     minArr.push(min * count);
-//     maxArr.push(max * count);
+console.log(legendre(7, 98), 16);
 
-//     count++;
+console.log(legendre(11, 500), 49);
+
+console.log(legendre(3, 600), 297);
+
+console.log(legendre(7, 6), 0);
+
+console.log(legendre(3, 1385), 688);
+
+console.log(legendre(5, 4324), 1077);
+
+console.log(legendre(2, 8663), 8655);
+
+// function legendre(p, n) {
+//   if (p > n) return 0;
+
+//   let count = 0;
+//   for (let i = 1; p ** i <= n; i++) {
+//     count += ~~(n / p ** i);
 //   }
 
-//   return minArr.filter((item) => maxArr.includes(item)).join("");
-// };
+//   return count;
+// }
