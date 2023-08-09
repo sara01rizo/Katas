ac3b217dd41e44cf75fc915c9e4cb1882e58d0bd
sara pulido <sara01rizo@gmail.com>
2023-08-09 14:24:18 +0200

    Add: mean js file

---
+++ mean.js
@@ -0,0 +1,24 @@
+// Solution V1
+function mean(num) {
+  return [...String(num)].reduce((a, c) => a + +c, 0) / String(num).length;
+}
+
+console.log(mean(666));
+
+console.log(mean(80));
+
+console.log(mean(789));
+
+console.log(mean(417));
+
+console.log(mean(1357));
+
+console.log(mean(42));
+
+console.log(mean(12345));
+
+// function mean(n) {
+//   let arr = n.toString().split("");
+
+//   return arr.reduce((prev, curr) => prev + Number(curr), 0) / arr.length;
+// }
