ac837ca806fe049d8ef44967bc4a3c57048e0d28
sara pulido <sara01rizo@gmail.com>
2023-08-19 16:43:13 +0200

    Add: repeater js file

---
+++ repeater.js
@@ -0,0 +1,23 @@
+// Solution V1
+function repeater(str, num) {
+  s = "";
+  for (let i = 0; i < num; i++) {
+    s += str;
+  }
+  return s;
+}
+
+console.log(repeater("A", 268435440));
+console.log(repeater("A", 10000000));
+console.log(repeater("A", 100));
+console.log(repeater("A", 10));
+console.log(repeater("A", 1));
+console.log(repeater("-", 1));
+console.log(repeater("-", 10));
+console.log(repeater("-", 100));
+console.log(repeater("/", 268435440));
+console.log(repeater("!", 268435440));
+console.log(repeater("?", 268435440));
+console.log(repeater("°", 268435440));
+
+// const repeater = (str, num) => (num <= 0 ? "" : Boolean(str.repeat(num)));
