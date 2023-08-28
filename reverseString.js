de39aa32da897895d2efbeb426fcb7e70c7ad514
sara pulido <sara01rizo@gmail.com>
2023-08-28 22:37:14 +0200

    Add: reverse String js file

---
+++ reverseString.js
@@ -0,0 +1,8 @@
+const reverseString = (str) => str.split("").reverse().join("");
+
+console.log(reverseString("hello"));
+console.log(reverseString("world"));
+console.log(reverseString(""));
+console.log(reverseString("abcdefg"));
+console.log(reverseString("123456789"));
+console.log(reverseString("linux"));
