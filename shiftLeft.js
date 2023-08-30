60ca91727ddb7a5fd60b18dbeb3b3c1142f03439
sara pulido <sara01rizo@gmail.com>
2023-08-30 20:35:20 +0200

    Add: shiftLeft js file

---
+++ shiftLeft.js
@@ -0,0 +1,14 @@
+// Solution V1
+const shiftLeft = (x, y) => x * 2 ** y;
+
+console.log(shiftLeft(5, 2));
+console.log(shiftLeft(10, 3));
+console.log(shiftLeft(-32, 2));
+console.log(shiftLeft(-6, 5));
+console.log(shiftLeft(12, 4));
+console.log(shiftLeft(46, 6));
+
+// Solution V2
+// const shiftLeft = (x, y) => {
+//   return x * Math.pow(2, y);
+// };
